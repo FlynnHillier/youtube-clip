47213dbfd76442772ce2d9541042fcabcd05929d
FlynnHillier <flynnhillier@gmail.com>
2023-07-12 00:33:13 +0100

    added readme; whisper_transcription file;

---
+++ readme.md
@@ -0,0 +1,2 @@
+### pre-requisites 
+install ffmpeg and add bin folder to PATH enviroment variable
---
+++ whisper_transcription.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+import whisper
+
+model = whisper.load_model("base.en")
